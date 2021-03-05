5485fe15ecf114343237b973cb5d40b021a7459a
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-05 14:22:24 +0100

    solving js-ex3

--- js-ex3/exercise.js
+++ js-ex3/exercise.js
@@ -7,6 +7,14 @@ formula(5) //=> 6
 formula() //=> 2
 
 // review all the possible parameters
-function formula(){
-
+function formula(x ,y ,z){
+    var result = 0;
+    if (x > 0 && y > 0  && z > 0) {
+        result = (x*y) + (z);
+    } else if (x > 0 && y > 0  && typeof z === "undefined") {
+        result = (x*y) + (1);
+    } else if (x > 0 && typeof y === "undefined"  && typeof z === "undefined") {
+        result = x + 1;
+    } else { result = 1 + 1; }
+    console.log(result);
 }
