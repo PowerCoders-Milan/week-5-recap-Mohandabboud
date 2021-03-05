3d36ba7983b41837566d22cb17fb407f489cf00c
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-05 18:27:00 +0100

    solving dom-extra

--- dom-extra/exercise.js
+++ dom-extra/exercise.js
@@ -46,17 +46,61 @@ number = 0;
 function sol() {
     var body = document.querySelector("body");
     let p = document.createElement("div");
-    console.log(Movies[0].title);
-    console.log(Movies[1].title);
-    console.log(Movies[2].title);
-    console.log(Movies[3].title);
     p.innerHTML = Movies[number].title +" , "+Movies[number].duration +" , "+Movies[number].actor;
     body.appendChild(p);
+    function color (){
+        p.style.backgroundColor= "black"
+        p.style.color= "white"
+    }
+    function color1 (){
+        p.style.backgroundColor= "white"
+        p.style.color= "black"
+    }
+    if (Movies[number].duration > 130 ) {
+        function duration () {
+        p.style.backgroundColor= "green"
+        }
+    }
+    console.log(Movies[number].duration);
+    p.addEventListener("mouseenter", color);
+    p.addEventListener("mouseleave", color1);
+    p.addEventListener("click",  duration);
     number++ ;
+    if (number > 3) {
+        console.log("this is condition")
+        button.removeEventListener("click", sol);
+    }
+    
 };
-console.log(number);
 button.addEventListener("click", sol);
-if (number > 4) {
-button.removeEventListener("click", sol);
-}
 
+
+
+
+/*
+function color (){
+    p[0].style.backgroundColor= "black"
+    p[0].style.color= "white"
+    console.log(p[0])
+}
+function color2 (){
+    var div2 = document.querySelectorAll("p")[1];
+    div2.style.backgroundColor= "black"
+    div2.style.color= "white"
+}
+function color3 (){
+    var div3 = document.querySelectorAll("p")[2];
+    div3.style.backgroundColor= "black"
+    div3.style.color= "white"
+}
+function color4 (){
+    var div4 = document.querySelectorAll("p")[3];
+    div4.style.backgroundColor= "black"
+    div4.style.color= "white"
+}
+p[0].addEventListener("mouseover",color1);
+div2.addEventListener("mouseover",color2);
+div3.addEventListener("mouseover",color3);
+div4.addEventListener("mouseover",color4);
+
+*/
